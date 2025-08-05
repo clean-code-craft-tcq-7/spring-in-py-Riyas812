e35dc593681740b06b656130dbe812308d6e04a9
Riyas812 <mohamedriyasm1700@gmail.com>
2025-08-05 18:32:10 +0530

    Update stats2

--- stats.py
+++ stats.py
@@ -7,7 +7,7 @@ def calculateStats(numbers):
     stats['avg'] = sum(numbers)/len(numbers)
     stats['max'] = max(numbers)
     stats['min'] = min(numbers)
-  else  
+  else:  
     stats['avg'] = float('nan')
     stats['max'] = float('nan')
     stats['min'] = float('nan')    
